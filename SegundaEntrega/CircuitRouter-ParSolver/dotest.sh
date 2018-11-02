7d901b7ffb2e86e3a6318a5185f02412640486af
decasppereira <43865518+decasppereira@users.noreply.github.com>
2018-11-02 14:36:22 +0000

    Add files via upload

---
+++ SegundaEntrega/CircuitRouter-ParSolver/dotest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TIMEFORMAT=%R #Time now returns only the real time
+
+
+#Filename handling and generating output filename
+filename=$(basename $2)
+output="../results/"$filename".speedups.csv"
+input="../"$2
+exectime=$input".res"
+touch $output
+echo "#threads,exec_time,speedup" > $output
+
+#Sequencial Solver time and print to file for reference
+../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver "$input">/dev/null
+seqtime=$(cut -c 19-27 <(grep "Elapsed" $exectime))
+
+echo "1S,$seqtime,1" >> $output
+
+#Testing all thread numbers
+for i in $(seq 1 $1); do
+    ./CircuitRouter-ParSolver -t $1 $input >/dev/null 
+    partime=$(cut -c 19-27 <(grep "Elapsed" $exectime))
+    speedup=$(echo "scale=6; ${seqtime}/${partime}" | bc)
+    echo "$i,$partime,$speedup" >> $output
+done
