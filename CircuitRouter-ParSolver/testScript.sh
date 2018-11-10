5e811b1f1ca87cca9e8a54108229baad2567f09f
decasppereira <43865518+decasppereira@users.noreply.github.com>
2018-11-10 02:00:16 +0000

    Queremos MORRER

---
+++ CircuitRouter-ParSolver/testScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+errcnt=0
+total=$(($2 * $1))
+file=inputs/random-x32-y32-z3-n64.txt
+for i in $(seq 1 $2); do
+    ./dotest.sh $1 $file &> /dev/null
+    errcnt=$((errcnt + $(grep -c ",," ../results/$(basename $file).speedups.csv) ))
+done
+
+success=$((total - errcnt))
+percentage=$(echo "scale=2; ${success}*100/${total}" | bc)
+
+echo errors: $errcnt
+echo sucesses: $success
+echo percentage: ${percentage}%
