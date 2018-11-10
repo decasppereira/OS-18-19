9db73fb02318378e775f8a350cf4247a6f632183
decasppereira <43865518+decasppereira@users.noreply.github.com>
2018-11-10 17:50:16 +0000

    Add files via upload

--- SegundaEntrega/CircuitRouter-ParSolver/doTest.sh
+++ SegundaEntrega/CircuitRouter-ParSolver/doTest.sh
@@ -12,14 +12,14 @@ execTime=$input".res"
 echo "#threads,exec_time,speedup" > $output
 
 ../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver "$input">/dev/null
-seqTime=$(cut -c 19-27 <(grep "Elapsed" $execTime))    # Duration of SeqSolver
+seqTime=$(cut -c 19-26 <(grep "Elapsed" $execTime))    # Duration of SeqSolver
 
-echo "1S, $seqTime, 1" >> $output
+echo "1S,$seqTime,1" >> $output
 
 # Speedup for each number of threads
 for ((i = 1; i <= $numThreads; i++)); do
     ./CircuitRouter-ParSolver -t $numThreads $input >/dev/null
-    parTime=$(cut -c 19-27 <(grep "Elapsed" $execTime))
+    parTime=$(cut -c 19-26 <(grep "Elapsed" $execTime))
     speedup=$(echo "scale=6; ${seqTime}/${parTime}" | bc)
-    echo "$i, $parTime, $speedup" >> $output
+    echo "$i,$parTime,$speedup" >> $output
 done
